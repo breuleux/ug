6cbf5c249eaff76281039bda6166aabd2949f920
Olivier Breuleux <breuleux@gmail.com>
2013-05-22 17:53:55 -0400

    Pelican configuration and theme

---
+++ doc/pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Olivier Breuleux'
+SITENAME = u'Unholy Grail documentation'
+SITEURL = ''
+
+THEME = 'theme'
+
+TIMEZONE = 'North America/Montreal'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Unholy Grail', 'https://github.com/breuleux/ug'),
+          ('Python.org', 'http://python.org/'))
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
